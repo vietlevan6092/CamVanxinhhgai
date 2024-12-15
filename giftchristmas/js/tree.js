51d59dc835f3d7034ac51fd527162017986fbdb0
Lê Văn Việt <vanviet.education.2003@gmail.com>
2024-12-15 18:51:02 +0900

    Delete giftchristmas directory

--- giftchristmas/js/tree.js
+++
@@ -1,9 +0,0 @@
-// click on the tree to see the animation again. 
-
-$(document).ready(function(){
-    $('#christmas-tree').mouseleave(function(){
-      $(this).removeClass('clicked');
-    }).click(function(){
-      $(this).addClass('clicked').html($(this).html());
-    });
-  });
